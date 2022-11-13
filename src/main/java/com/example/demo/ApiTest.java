201570d21d9646fa8104364a44d1fe730c29f533
Hsieh, Pei-Tsang <hsiehpeitsang@gmail.com>
2022-11-14 00:59:21 +0800

    微調一下看看Jenkins效果

--- src/main/java/com/example/demo/ApiTest.java
+++ src/main/java/com/example/demo/ApiTest.java
@@ -11,6 +11,7 @@ public class ApiTest {
 
     @PostMapping("/postbody")
     public String postBody(@RequestBody String fullName) {
+        System.out.println("Receive post body -> " + fullName);
         return "Hello " + fullName;
     }
 }
